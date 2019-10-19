36c3837614472231d38eeb87139afa88a21a405f
emilyyleung <30677901+emilyyleung@users.noreply.github.com>
2019-10-19 11:08:50 +1100

    List Difference works!!

--- django_project/list_difference/forms.py
+++ django_project/list_difference/forms.py
@@ -6,11 +6,11 @@ class ListCheckerForm(forms.Form):
     # listA = forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':100}), label="Enter list A", max_length=99999) # If you want to control rows and columns
     # listB = forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':100}), label="Enter list B", max_length=99999)
 
-    listA = forms.CharField(widget=forms.Textarea, label="Enter list A", max_length=99999)
-    listB = forms.CharField(widget=forms.Textarea, label="Enter list B", max_length=99999)
+    listA = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'id': 'exampleFormControlTextarea3'}), label="Enter list A", max_length=99999)
+    listB = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'id': 'exampleFormControlTextarea3'}	), label="Enter list B", max_length=99999)
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.helper = FormHelper
-        self.helper.form_method = "post"
+        # self.helper = FormHelper
+        # self.helper.form_method = "POST"
